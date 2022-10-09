139aef7a08cb09df2333293ef9e5bb177f042c12
Samuel Daza <65508457+samueldazadaza@users.noreply.github.com>
2022-10-09 10:43:00 -0500

    Add 3 tablas

--- index.js
+++ index.js
@@ -3,10 +3,56 @@ const url = 'https://api-rigel.herokuapp.com'
 //CODIGO PARA PINTAR DATOS EN HTML
 fetch(url)
   .then(response => response.json())
-  .then(data => mostrarData(data))
+  .then(data => mostrarDataIts(data))
+  .then(data => mostrarDataSirci(data))
+  .then(data => mostrarDataTotal(data))
   .catch(error => console.log(error))
 
-const mostrarData = (data) => {
+const mostrarDataIts = (data) => {
+    const datarigel = data.data.data
+    const datarigelits = datarigel.filter(item => item.system_name === 'IT-ITS')
+    const datarigelsirci = datarigel.filter(item => item.system_name === 'IT-SIRCI')
+    const areaitbuses = [...datarigelits, ...datarigelsirci] //operador de propagación para fusionar objetos en JavaScript
+  let body = ''
+  for (let i = 0; i < datarigelits.length; i++){
+    body += `
+      <tr>
+        <td> ${i + 1} </td>
+        <td> ${datarigelits[i].vehicle_code} </td>
+        <td> ${datarigelits[i].system_name} </td>
+        <td> ${datarigelits[i].issue_description} </td>
+        <td> ${datarigelits[i].date_created} </td>
+        <td> ${datarigelits[i].days_off} </td>
+        <td> ${datarigelits[i].is_inmovilized_vehicle} </td>
+      </tr>
+    `
+  }
+  document.getElementById("dataits").innerHTML = body //para pintar datos en html
+}
+
+const mostrarDataSirci = (data) => {
+    const datarigel = data.data.data
+    const datarigelits = datarigel.filter(item => item.system_name === 'IT-ITS')
+    const datarigelsirci = datarigel.filter(item => item.system_name === 'IT-SIRCI')
+    const areaitbuses = [...datarigelits, ...datarigelsirci] //operador de propagación para fusionar objetos en JavaScript
+  let body = ''
+  for (let i = 0; i < datarigelsirci.length; i++){
+    body += `
+      <tr>
+        <td> ${i + 1} </td>
+        <td> ${datarigelsirci[i].vehicle_code} </td>
+        <td> ${datarigelsirci[i].system_name} </td>
+        <td> ${datarigelsirci[i].issue_description} </td>
+        <td> ${datarigelsirci[i].date_created} </td>
+        <td> ${datarigelsirci[i].days_off} </td>
+        <td> ${datarigelsirci[i].is_inmovilized_vehicle} </td>
+      </tr>
+    `
+  }
+  document.getElementById("datasirci").innerHTML = body //para pintar datos en html
+}
+
+const mostrarDataTotal = (data) => {
     const datarigel = data.data.data
     const datarigelits = datarigel.filter(item => item.system_name === 'IT-ITS')
     const datarigelsirci = datarigel.filter(item => item.system_name === 'IT-SIRCI')
@@ -25,5 +71,6 @@ const mostrarData = (data) => {
       </tr>
     `
   }
-  document.getElementById("data").innerHTML = body //para pintar datos en html
+  document.getElementById("datatotal").innerHTML = body //para pintar datos en html
 }
+
