f9c851cacb7a246e62e52cdb09518d6df99733e7
Samuel Daza <65508457+samueldazadaza@users.noreply.github.com>
2022-11-05 12:32:21 -0500

    add js areas data

--- index.js
+++ index.js
@@ -10,13 +10,16 @@ fetch(url)
 const mostrarDataTotal = (data) => {
     const datarigel = data.reporteRigel.inoperativos.data
     const sum = data.reporteRigel.inoperativosSuma
-    console.log(data)
-    console.log(datarigel)
+    //console.log(data)
+    //console.log(datarigel)
     //contar cuantos elementos hay de cada area: "system_name"
     const arr = datarigel
-    console.log(arr)
+    //console.log(arr)
     var resultado = arr.reduce( (acc, arr) => (acc[arr.system_name] = (acc[arr.system_name] || 0) + 1, acc), {} );
     console.log(resultado)
+    
+    const arr2 = arr.map(elemento => Object.entries(elemento));
+    console.log(arr2)
 
     //filtrar area its en general
     const datarigelits = datarigel.filter(item => item.system_name === 'IT-ITS')
@@ -70,49 +73,30 @@ const mostrarDataTotal = (data) => {
           <td> ${datarigel[i].days_off} </td>
           <td> ${datarigel[i].is_inmovilized_vehicle} </td>
           <td> ${datarigel[i].current_status} </td>
-</tr>
+        </tr>
       `
     }
 
  //pintar data suma
-
-    let bodysuma = ''
-
-    for (let i = 0; i < sum.length; i++){
-
+ let bodysuma = ''
+ 
+ for (let i = 0; i < sum.length; i++){
+   
+   console.log("comienza apintar data suma")
+   //console.log(i)
+   //console.log(sum[i][0])
+   //console.log(sum[i][1])
       bodysuma += `
-
         <tr>
-
           <td> ${i + 1} </td>
-
           <td> ${sum[i][0] } </td>
           <td> ${sum[i][1] } </td>
-
-     
-
-
-
- 
-
-         
-
-
-
         </tr>
-
       `
-
     }
 
-
-
-
-
-          
-  
   document.getElementById("dataits").innerHTML = bodyits //para pintar datos en html
   document.getElementById("datasirci").innerHTML = bodysirci //para pintar datos en html
   document.getElementById("datatotal").innerHTML = bodytotal //para pintar datos en html
-  document.getElementById("dataprueba").innerHTML = bodysuma //para pintar datos en html
+  document.getElementById("sumaporarea").innerHTML = bodysuma //para pintar datos en html
 }
