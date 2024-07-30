6699462d60602e3d19cb257e5d044cbd7a1b3961
Samuel Daza <65508457+samueldazadaza@users.noreply.github.com>
2024-07-29 19:30:07 -0500

    agregar dos columnas js

--- index.js
+++ index.js
@@ -15,44 +15,93 @@ const datosGenerales = datos.data;
 console.log(datosGenerales)
 
 //filtrar area its en general
+// FILTRO DATOS ITS GENERAL, Z63 Y Z67
 const datosIts = datosGenerales.filter(item => item.system_name === 'IT-ITS') //filtrar buses its
+console.log(datosIts);
+const datosItsZ63 = datosIts.filter(item => item.uf_code === 'ZMO III') //filtrar buses its Z63
+const datosItsZ67 = datosIts.filter(item => item.uf_code === 'ZMO V') //filtrar buses its Z67
+
+// FILTRO DATOS SIRCI GENERAL, Z63 Y Z67
 const datosSirci = datosGenerales.filter(item => item.system_name === 'IT-SIRCI') //filtrar buses sirci
+console.log(datosSirci);
+const datosSirciZ63 = datosSirci.filter(item => item.uf_code === 'ZMO III') //filtrar buses its Z63
+const datosSirciZ67 = datosSirci.filter(item => item.uf_code === 'ZMO V') //filtrar buses its Z67
+
 //const areaitbuses = [...datarigelits, ...datarigelsirci] //operador de propagación para fusionar objetos en JavaScript
 
 
-    //datosIts
-    let bodyits = ''
-  for (let i = 0; i < datosIts.length; i++){
-    bodyits += `
-      <tr>
-        <td> ${i + 1} </td>
-        <td> ${datosIts[i].system_name} </td>
-        <td> ${datosIts[i].vehicle_code} </td>
-        <td> ${datosIts[i].issue_description} </td>
-        <td> ${datosIts[i].date_created} </td>
-        <td> ${datosIts[i].days_off} </td>
-        <td> ${datosIts[i].is_inmovilized_vehicle} </td>
-        <td> ${datosIts[i].current_status} </td>
-      </tr>
-    `
-  }
-
-      //datosSirci
-      let bodysirci = ''
-      for (let i = 0; i < datosSirci.length; i++){
-        bodysirci += `
-          <tr>
-            <td> ${i + 1} </td>
-            <td> ${datosSirci[i].system_name} </td>
-            <td> ${datosSirci[i].vehicle_code} </td>
-            <td> ${datosSirci[i].issue_description} </td>
-            <td> ${datosSirci[i].date_created} </td>
-            <td> ${datosSirci[i].days_off} </td>
-            <td> ${datosSirci[i].is_inmovilized_vehicle} </td>
-            <td> ${datosSirci[i].current_status} </td>
-          </tr>
-        `
+    //datosItsZ63
+    let bodyitsZ63 = ''
+    for (let i = 0; i < datosItsZ63.length; i++)
+      {
+      bodyitsZ63 += `
+        <tr>
+          <td> ${i + 1} </td>
+          <td> ${datosItsZ63[i].system_name} </td>
+          <td> ${datosItsZ63[i].vehicle_code} </td>
+          <td> ${datosItsZ63[i].issue_description} </td>
+          <td> ${datosItsZ63[i].date_created} </td>
+          <td> ${datosItsZ63[i].days_off} </td>
+          <td> ${datosItsZ63[i].is_inmovilized_vehicle} </td>
+          <td> ${datosItsZ63[i].current_status} </td>
+        </tr>
+      `
       }
+
+    //datosItsZ67
+    let bodyitsZ67 = ''
+    for (let i = 0; i < datosItsZ67.length; i++)
+      {
+      bodyitsZ67 += `
+        <tr>
+          <td> ${i + 1} </td>
+          <td> ${datosItsZ67[i].system_name} </td>
+          <td> ${datosItsZ67[i].vehicle_code} </td>
+          <td> ${datosItsZ67[i].issue_description} </td>
+          <td> ${datosItsZ67[i].date_created} </td>
+          <td> ${datosItsZ67[i].days_off} </td>
+          <td> ${datosItsZ67[i].is_inmovilized_vehicle} </td>
+          <td> ${datosItsZ67[i].current_status} </td>
+        </tr>
+      `
+      }
+
+    //datossirciZ63
+    let bodysirciZ63 = ''
+    for (let i = 0; i < datosSirciZ63.length; i++)
+      {
+      bodysirciZ63 += `
+        <tr>
+          <td> ${i + 1} </td>
+          <td> ${datosSirciZ63[i].system_name} </td>
+          <td> ${datosSirciZ63[i].vehicle_code} </td>
+          <td> ${datosSirciZ63[i].issue_description} </td>
+          <td> ${datosSirciZ63[i].date_created} </td>
+          <td> ${datosSirciZ63[i].days_off} </td>
+          <td> ${datosSirciZ63[i].is_inmovilized_vehicle} </td>
+          <td> ${datosSirciZ63[i].current_status} </td>
+        </tr>
+      `
+      }
+
+    //datossirciZ67
+    let bodysirciZ67 = ''
+    for (let i = 0; i < datosSirciZ67.length; i++)
+      {
+      bodysirciZ67 += `
+        <tr>
+          <td> ${i + 1} </td>
+          <td> ${datosSirciZ67[i].system_name} </td>
+          <td> ${datosSirciZ67[i].vehicle_code} </td>
+          <td> ${datosSirciZ67[i].issue_description} </td>
+          <td> ${datosSirciZ67[i].date_created} </td>
+          <td> ${datosSirciZ67[i].days_off} </td>
+          <td> ${datosSirciZ67[i].is_inmovilized_vehicle} </td>
+          <td> ${datosSirciZ67[i].current_status} </td>
+        </tr>
+      `
+      }
+    
 
       //datosTotal
       let bodytotal = ''
@@ -89,13 +138,20 @@ const datosSirci = datosGenerales.filter(item => item.system_name === 'IT-SIRCI'
                   <td> ${ valorcontadorareas[i] } </td>
                 </tr>
               `
-        }
+        };
       
 
 //pintar datos en el html
-  document.getElementById("dataits").innerHTML = bodyits //para pintar datos en html
-  document.getElementById("datasirci").innerHTML = bodysirci //para pintar datos en html
-  document.getElementById("datatotal").innerHTML = bodytotal //para pintar datos en html
-  document.getElementById("datasuma").innerHTML = bodysuma //para pintar datos en html
-  document.getElementById("loader").style.display = "none"; //para ocultar loader al cargar pagina web
+  //datos ITS
+  document.getElementById("dataitsZ63").innerHTML = bodyitsZ63; //para pintar datos en html
+  document.getElementById("dataitsZ67").innerHTML = bodyitsZ67 //para pintar datos en html
+  //datos SIRCI
+  document.getElementById("datasirciZ63").innerHTML = bodysirciZ63 //para pintar datos en html
+  document.getElementById("datasirciZ67").innerHTML = bodysirciZ67 //para pintar datos en html
+  
+  //pintar otro datos
+   //document.getElementById("datasirci").innerHTML = bodysirci //para pintar datos en html
+   document.getElementById("datatotal").innerHTML = bodytotal //para pintar datos en html
+   document.getElementById("datasuma").innerHTML = bodysuma //para pintar datos en html
+ document.getElementById("loader").style.display = "none"; //para ocultar loader al cargar pagina web
 }
